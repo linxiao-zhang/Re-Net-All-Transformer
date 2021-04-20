6ed53717f88566138319779910ff60f39af6c3b1
ZLX <1456597572@qq.com>
2021-04-20 13:15:10 +0800

    修改Informer

--- informer_model.py
+++ informer_model.py
@@ -69,7 +69,7 @@ class Informer(nn.Module):
     def forward(self, x_enc, x_mark_enc,
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         enc_out = self.linear(x_enc)
-        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        # enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
         dec_out = enc_out
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
         # return dec_out[:, -self.pred_len:, :]
